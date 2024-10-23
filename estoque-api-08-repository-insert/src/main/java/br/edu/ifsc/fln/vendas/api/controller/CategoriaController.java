374a1250c26c12803c0649c2c2fa7678f95d6e93
Marcos André Pisching <marcos.pisching@ifsc.edu.br>
2024-10-23 16:49:43 -0300

    Estoque-api - método para inserção de dados Categoria com HTTP POST
    uso de requisição POST

---
+++ estoque-api-08-repository-insert/src/main/java/br/edu/ifsc/fln/vendas/api/controller/CategoriaController.java
@@ -0,0 +1,53 @@
+package br.edu.ifsc.fln.vendas.api.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.ifsc.fln.vendas.model.domain.Categoria;
+import br.edu.ifsc.fln.vendas.repository.CategoriaRepository;
+
+@RestController
+public class CategoriaController {
+	private final CategoriaRepository categoriaRepository;
+	
+	public CategoriaController(CategoriaRepository categoriaRepository) {
+		this.categoriaRepository = categoriaRepository;
+	}
+
+	@GetMapping("/categorias")
+	public List<Categoria> listar() {
+		return categoriaRepository.findAll();
+	}
+	
+	@GetMapping("/categorias/{id}")
+	public ResponseEntity<Categoria> pesquisar(@PathVariable Integer id) {
+		return categoriaRepository.findById(id)
+				.map(ResponseEntity::ok) 
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/categorias/descricao/{descricao}")
+	public ResponseEntity<List<Categoria>> pesquisar(@PathVariable String descricao) {
+		List<Categoria> categorias = categoriaRepository.findByDescricao(descricao);
+		if (categorias.size() > 0) {
+			return ResponseEntity.ok(categorias);
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping("/categorias")
+	@ResponseStatus(HttpStatus.CREATED) //devolve o status 201
+	public Categoria inserir(@RequestBody Categoria categoria) {
+		return categoriaRepository.save(categoria);
+	}
+}
