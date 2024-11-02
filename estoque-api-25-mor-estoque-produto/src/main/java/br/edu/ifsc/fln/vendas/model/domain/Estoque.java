0f823972ed067a19d01ed609eca7ed4fa9bd9b74
Marcos André Pisching <marcos.pisching@ifsc.edu.br>
2024-11-02 12:51:12 -0300

    Estoque - MOR com Produto

---
+++ estoque-api-25-mor-estoque-produto/src/main/java/br/edu/ifsc/fln/vendas/model/domain/Estoque.java
@@ -0,0 +1,91 @@
+package br.edu.ifsc.fln.vendas.model.domain;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Estoque {
+	@Id
+	private int id;
+	private int quantidade;//quantidade atual
+	private int quantidadeMaxima;
+	private int quantidadeMinima;
+	private ESituacao eSituacao = ESituacao.ATIVO;
+	
+	@OneToOne
+	@MapsId
+	@JoinColumn(name = "id") // O ID de Estoque será o mesmo que o de Produto
+	private Produto produto;
+	
+	public Estoque() {}
+
+    public Estoque(Produto produto) {
+        this.produto = produto;
+    }	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getQuantidadeMaxima() {
+		return quantidadeMaxima;
+	}
+	public void setQuantidadeMaxima(int quantidadeMaxima) {
+		this.quantidadeMaxima = quantidadeMaxima;
+	}
+	public int getQuantidadeMinima() {
+		return quantidadeMinima;
+	}
+	public void setQuantidadeMinima(int quantidadeMinima) {
+		this.quantidadeMinima = quantidadeMinima;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public ESituacao getESituacao() {
+		return eSituacao;
+	}
+	public void setESituacao(ESituacao eSituacao) {
+		this.eSituacao = eSituacao;
+	}
+	
+    public void repor(int quantidade) throws Exception {
+        if (this.quantidade + quantidade > this.quantidadeMaxima) {
+            throw new Exception("Quantidade excede o máximo permitido no estoque");
+        }
+        this.quantidade += quantidade;
+    }
+
+    public void retirar(int quantidade) throws Exception {
+        if (this.quantidade - quantidade < this.quantidadeMinima) {
+            throw new Exception("Quantidade abaixo do mínimo permitido no estoque");
+        }
+        this.quantidade -= quantidade;
+    }
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estoque other = (Estoque) obj;
+		return id == other.id;
+	}
+    
+    
+}
