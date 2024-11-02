bc09384564e2db5bd98b90d15f7c429b8de41bf6
Marcos André Pisching <marcos.pisching@ifsc.edu.br>
2024-11-02 11:21:08 -0300

    Estoque - migration flyway

---
+++ estoque-api-24-migration-estoque/src/main/java/br/edu/ifsc/fln/vendas/api/controller/ProdutoController.java
@@ -0,0 +1,91 @@
+package br.edu.ifsc.fln.vendas.api.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.ifsc.fln.vendas.model.domain.Categoria;
+import br.edu.ifsc.fln.vendas.model.domain.Fornecedor;
+import br.edu.ifsc.fln.vendas.model.domain.Produto;
+import br.edu.ifsc.fln.vendas.repository.CategoriaRepository;
+import br.edu.ifsc.fln.vendas.repository.FornecedorRepository;
+import br.edu.ifsc.fln.vendas.repository.ProdutoRepository;
+
+@RestController //Componente Spring com semântica de controlador
+public class ProdutoController {
+
+	private ProdutoRepository produtoRepository;
+	private CategoriaRepository categoriaRepository;
+	private FornecedorRepository fornecedorRepository;
+	
+	public ProdutoController(ProdutoRepository produtoRepository, 
+			                 CategoriaRepository categoriaRepository,
+			                 FornecedorRepository fornecedorRepository) {
+		this.produtoRepository = produtoRepository;
+		this.categoriaRepository = categoriaRepository;
+		this.fornecedorRepository = fornecedorRepository;
+	}
+	
+	@GetMapping("/produtos")
+	public List<Produto> listar() {
+		return produtoRepository.findAll();
+	}
+	//recurso para pesquisar produto por id
+	@GetMapping("/produtos/{id}")
+	public ResponseEntity<Produto> pesquisar(@PathVariable Integer id) {
+		return produtoRepository.findById(id)
+				.map(ResponseEntity::ok) 
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping("/produtos")
+	@ResponseStatus(HttpStatus.CREATED) 
+	public Produto inserir(@RequestBody Produto produto) {
+		produto = produtoRepository.save(produto);
+		//vamos melhorar a instrução abaixo em outra oportunidade
+		//Localização da categoria por meio do id da categoria
+        // associado a produto.
+		Optional<Categoria> c = categoriaRepository.findById(produto.getCategoria().getId());
+		produto.setCategoria(c.get());
+		Optional<Fornecedor> f = fornecedorRepository.findById(produto.getFornecedor().getId());
+		produto.setFornecedor(f.get());
+		return produto;
+	}
+	
+	@PutMapping("/produtos/{id}")
+	public ResponseEntity<Produto> atualizar(@PathVariable Integer id,
+											   @RequestBody Produto produto) {
+		
+		if (!produtoRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		} else {
+			produto.setId(id);
+			Produto entidadeAtualizada = produtoRepository.save(produto);
+			Optional<Categoria> c = categoriaRepository.findById(entidadeAtualizada.getCategoria().getId());
+			entidadeAtualizada.setCategoria(c.get());
+			Optional<Fornecedor> f = fornecedorRepository.findById(entidadeAtualizada.getFornecedor().getId());
+			entidadeAtualizada.setFornecedor(f.get());			
+			return ResponseEntity.ok(entidadeAtualizada);
+		}
+	}	
+	
+	@DeleteMapping("/produtos/{id}")
+	public ResponseEntity<Void> remover(@PathVariable Integer id) {
+		if (!produtoRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		} else {
+			produtoRepository.deleteById(id);
+			return ResponseEntity.noContent().build();
+		}
+	}
+}
---
+++ estoque-api-24-migration-estoque/src/main/java/br/edu/ifsc/fln/vendas/model/domain/Produto.java
@@ -0,0 +1,97 @@
+package br.edu.ifsc.fln.vendas.model.domain;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String descricao;
+	private BigDecimal preco;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_categoria")
+	private Categoria categoria;
+	@ManyToOne
+	@JoinColumn(name = "id_fornecedor")
+	@JsonIgnoreProperties("produtos")
+	private Fornecedor fornecedor;
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	public Fornecedor getFornecedor() {
+		return fornecedor;
+	}
+
+	public void setFornecedor(Fornecedor fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
