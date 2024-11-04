2146141235eb38c157d9c9826b4a0508f97e6690
Marcos André Pisching <marcos.pisching@ifsc.edu.br>
2024-11-04 17:13:26 -0300

    Estoque - movimentação
    endpoints para retirar e repor

---
+++ estoque-api-26-estoque-movimentacao/src/main/java/br/edu/ifsc/fln/vendas/model/domain/Estoque.java
@@ -0,0 +1,85 @@
+package br.edu.ifsc.fln.vendas.model.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Estoque {
+	@Id
+	@Column(name = "id_produto")
+	private int id;
+	private int quantidade;//quantidade atual
+	@Column(name = "qtd_maxima")
+	private int quantidadeMaxima;
+	@Column(name = "qtd_minima")
+	private int quantidadeMinima;
+	@Enumerated(EnumType.STRING)
+	@Column(name = "situacao")
+	private ESituacao eSituacao = ESituacao.ATIVO;
+	
+    @OneToOne
+    @MapsId // Garante que o id do Estoque seja igual ao id do Produto
+    @JoinColumn(name = "id_produto")
+    private Produto produto;
+	
+    // Método para associar o Produto ao Estoque
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }  
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getQuantidadeMaxima() {
+		return quantidadeMaxima;
+	}
+	public void setQuantidadeMaxima(int quantidadeMaxima) {
+		this.quantidadeMaxima = quantidadeMaxima;
+	}
+	public int getQuantidadeMinima() {
+		return quantidadeMinima;
+	}
+	public void setQuantidadeMinima(int quantidadeMinima) {
+		this.quantidadeMinima = quantidadeMinima;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public ESituacao getESituacao() {
+		return eSituacao;
+	}
+	public void setESituacao(ESituacao eSituacao) {
+		this.eSituacao = eSituacao;
+	}
+	
+    public void repor(int quantidade) throws Exception {
+    	if (this.eSituacao != ESituacao.ATIVO) {
+            throw new IllegalStateException("Estoque inativo ou bloqueado, retirada não permitida.");
+        }
+    	if (this.quantidade + quantidade > this.quantidadeMaxima) {
+            throw new Exception("Quantidade excede o máximo permitido no estoque");
+        }
+        this.quantidade += quantidade;
+    }
+
+    public void retirar(int quantidade) throws Exception {
+    	if (this.eSituacao != ESituacao.ATIVO) {
+            throw new IllegalStateException("Estoque inativo ou bloqueado, retirada não permitida.");
+        }
+        if (this.quantidade - quantidade < 0) {
+            throw new Exception("Quantidade insuficiente no estoque");
+        }
+        this.quantidade -= quantidade;
+    }
+    
+    
+}
---
+++ estoque-api-26-estoque-movimentacao/src/main/java/br/edu/ifsc/fln/vendas/model/domain/Produto.java
@@ -0,0 +1,112 @@
+package br.edu.ifsc.fln.vendas.model.domain;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String descricao;
+	private BigDecimal preco;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_categoria")
+	private Categoria categoria;
+	@ManyToOne
+	@JoinColumn(name = "id_fornecedor")
+	@JsonIgnoreProperties("produtos")
+	private Fornecedor fornecedor;
+	
+	@OneToOne(mappedBy = "produto", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private Estoque estoque;	
+
+    public Produto() {
+        this.estoque = new Estoque(); // Associação por composição
+        this.estoque.setProduto(this);
+    }
+
+    public Estoque getEstoque() {
+        return estoque;
+    }
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	public Fornecedor getFornecedor() {
+		return fornecedor;
+	}
+
+	public void setFornecedor(Fornecedor fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
