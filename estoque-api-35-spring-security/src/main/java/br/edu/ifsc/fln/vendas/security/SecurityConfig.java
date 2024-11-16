695cad07e322d99c1b927ed41f4d42f40a084378
Marcos André Pisching <marcos.pisching@ifsc.edu.br>
2024-11-16 10:20:02 -0300

    Update SecurityConfig.java

--- estoque-api-35-spring-security/src/main/java/br/edu/ifsc/fln/vendas/security/SecurityConfig.java
+++ estoque-api-35-spring-security/src/main/java/br/edu/ifsc/fln/vendas/security/SecurityConfig.java
@@ -1,10 +1,6 @@
 package br.edu.ifsc.fln.vendas.security;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
